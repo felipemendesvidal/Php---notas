789a8c9569609908bac30cdf8579bc23e45a27b9
felipe <contatofelipevidalmendes@gmail.com>
2022-10-17 17:50:58 -0300

    ensinando echo hello world

--- fundamentos/index.php
+++ fundamentos/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>fundamentos </title>
 </head>
 <body>
     
@@ -17,7 +17,11 @@
 //crie uma nova bagulho do xdebug la vai ta create a new nao sei oque 
 // depois jaera da play caraio 
 // deixa de ser burro animal 
+//não esquecer de trocar para web server na depuração 
+// não esquecer de colocar o nome da pasta e do index
+//http://localhost:49197/fundamentos
 
+echo 'ola mundo ';
 
 ?>
 </body>
