60bb826deb51126f1635f81a8017b69c992de6c5
felipe <contatofelipevidalmendes@gmail.com>
2022-10-17 17:10:19 -0300

    turma php aulas

---
+++ fundamentos/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<?php 
+
+// abra o arquivo php
+// depois aperte fn + f5
+//crie uma nova bagulho do xdebug la vai ta create a new nao sei oque 
+// depois jaera da play caraio 
+// deixa de ser burro animal 
+
+
+?>
+</body>
+</html>
